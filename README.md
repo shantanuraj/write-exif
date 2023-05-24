ca9f78ad3fafd68fb5de2ad7c8e8621de670441f
Shantanu Raj <s@sraj.me>
2023-05-25 00:24:48 +0200

    feat(pkg): Add exif tool
      Usage: ./write-exif
    
      Updates the file creation/modified timestamps to match the given date.
    
      Copies the metadata from file names onto their EXIF data, applies to
      all `jpg` files in the specified directory. Defaults to the current directory.
    
      File name format: 2023-05-09-19-51-00-52°22'42.2"N 4°52'59.9"E.jpg
    
      YYYY-MM-DD-HH-MM-SS-DD°MM'SS.S"N DD°MM'SS.S"E.jpg
      [date]-[time]-[geo coordinates].jpg
    
      ### Setup
    
      I use [bun](https://github.com/oven-sh/bun) to run the script.
      But you can transpile the TypeScript code using `tsc` and then run it
      using `node`.
      Alternatively `ts-node` can be used as well.

---
+++ README.md
@@ -0,0 +1,23 @@
+# write-exif
+
+## Usage
+
+    ./write-exif [directory]
+
+Updates the file creation/modified timestamps to match the given date.
+
+Copies the metadata from file names onto their EXIF data, applies to
+all `jpg` files in the specified directory. Defaults to the current directory.
+
+File name format: 2023-05-09-19-51-00-52°22'42.2"N 4°52'59.9"E.jpg
+
+```
+YYYY-MM-DD-HH-MM-SS-DD°MM'SS.S"N DD°MM'SS.S"E.jpg # [date]-[time]-[geo coordinates].jpg
+```
+
+### Setup
+
+I use [bun](https://github.com/oven-sh/bun) to run the script.
+But you can transpile the TypeScript code using `tsc` and then run it
+using `node`.
+Alternatively `ts-node` can be used as well.
---
+++ write-exif.ts
@@ -0,0 +1,90 @@
+#!/usr/bin/env bun
+
+// SPDX-License-Identifier: MIT
+
+import { readFileSync, readdirSync, utimesSync, writeFileSync } from "fs";
+import * as piexif from "piexifjs";
+
+const args = process.argv.slice(2);
+let directory = ".";
+if (args.length > 0) {
+  directory = args[0];
+}
+
+const files = readdirSync(directory, { withFileTypes: true }).filter(
+  (f) => !f.isDirectory() && f.name.endsWith(".jpg")
+);
+
+const filepath = (file: string) => `${directory}/${file}`;
+
+// File name format: 2023-05-19-16-40-00-43°22'01.9"N 16°55'51.6"E.jpg
+for (const file of files) {
+  const metadata = {
+    date: file.name.slice(0, 10),
+    time: file.name.slice(11, 19).replace(/-/g, ":"),
+  };
+  const ts = new Date(`${metadata.date} ${metadata.time}`);
+  const coordinates = file.name.slice(20, -4).split(" ");
+
+  const jpeg = readFileSync(filepath(file.name));
+  const data = jpeg.toString("binary");
+
+  const exifObj = piexif.load(data);
+
+  // Add timestamp data
+  exifObj["Exif"][piexif.ExifIFD.DateTimeOriginal] = ts
+    .toISOString()
+    .replace(/T/, " ")
+    .replace(/\..+/, "");
+
+  // Extract lat, long and their respective hemisphere (N/S, E/W)
+  const lat = dms2dec(coordinates[0].substring(0, coordinates[0].length - 1)); // latitude
+  const long = dms2dec(coordinates[1].substring(0, coordinates[1].length - 1)); // longitude
+  const latRef = coordinates[0].slice(-1) == "N" ? "N" : "S";
+  const longRef = coordinates[1].slice(-1) == "E" ? "E" : "W";
+
+  // Convert decimal to exif format (rationals)
+  const latitude = dec2exif(lat);
+  const longitude = dec2exif(long);
+
+  // Add GPS data
+  exifObj["GPS"][piexif.GPSIFD.GPSLatitudeRef] = latRef;
+  exifObj["GPS"][piexif.GPSIFD.GPSLatitude] = latitude;
+  exifObj["GPS"][piexif.GPSIFD.GPSLongitudeRef] = longRef;
+  exifObj["GPS"][piexif.GPSIFD.GPSLongitude] = longitude;
+
+  const exifbytes = piexif.dump(exifObj);
+  const newData = piexif.insert(exifbytes, data);
+  const newJpeg = Buffer.from(newData, "binary");
+
+  writeFileSync(filepath(file.name), newJpeg);
+
+  // Modify the created and updated timestamps
+  // to match the timestamp of the photo
+  const time = ts.getTime() / 1000;
+  utimesSync(filepath(file.name), time, time);
+}
+
+// DMS (Degrees Minutes Seconds) to Decimal Degrees
+function dms2dec(dms: string) {
+  const match = dms.match(/(\d+)°(\d+)'(\d+(\.\d+)?)"/)!;
+  const degrees = Number(match[1]);
+  const minutes = Number(match[2]);
+  const seconds = Number(match[3]);
+  return degrees + minutes / 60 + seconds / 3600;
+}
+
+// Decimal Degrees to EXIF format (rationals)
+function dec2exif(dec: number) {
+  const absolute = Math.abs(dec);
+  const degrees = Math.floor(absolute);
+  const minutes = Math.floor((absolute - degrees) * 60);
+  const seconds = ((absolute - degrees - minutes / 60) * 3600 * 10000).toFixed(
+    0
+  ); // to 4 decimal places
+  return [
+    [degrees, 1],
+    [minutes, 1],
+    [seconds, 10000],
+  ];
+}
